ae0ce48f00243cfe647f4c4243c3edbeefb68c4a
Tohin <tohin214@gmail.com>
2023-09-18 00:19:35 +0600

    init

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:agroshield_flutter/ui/utils/image_manager.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,8 +6,48 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("Home"),),
+    return  Scaffold(
+      body: SafeArea(child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(onPressed: (){}, icon: const Icon(Icons.menu)),
+                  IconButton(onPressed: (){}, icon: const Icon(Icons.notifications)),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              const Text("Plant Quest",style: TextStyle(
+                color: Colors.cyan,
+                fontWeight: FontWeight.bold,
+                fontSize: 30
+              ),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(ImageManager.homeScanPicPNG,
+                    height: 200,
+                    width: 200,
+                  ),
+                  Container(
+                    height: 200,
+                    decoration: const BoxDecoration(
+                      color: Colors.cyan,
+                      borderRadius: BorderRadius.only(topLeft: Radius.circular(10),bottomLeft: Radius.circular(10))
+                    ),
+                    child: Text("dfgsdfg"),
+                  )
+                ],
+              )
+
+            ],
+          ),
+        ),
+      )),
     );
   }
 }
--- lib/ui/utils/image_manager.dart
+++ lib/ui/utils/image_manager.dart
@@ -6,5 +6,14 @@ class ImageManager{
   static const String onboardingHarvestPNG = "assets/onboarding/harvest.png";
   static const String onboardingExplorePNG = "assets/onboarding/explore.png";
 
+  static const String homeBananaLeafPNG = "assets/home/bananaLeaf.png";
+  static const String homeConsultantPNG = "assets/home/consultant.png";
+  static const String homeGrapeBlackRotPNG = "assets/home/grapeBlackRot.png";
+  static const String homeScanPicPNG = "assets/home/scanPic.png";
+  static const String homeStrawberryScorchPNG = "assets/home/strawberryScorch.png";
+
+  static const String avatarUserPNG = "assets/auth/avatarUser.png";
+  static const String loginPNG = "assets/auth/login.png";
+
 
 }
